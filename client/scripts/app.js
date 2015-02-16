7e7f3a06121d3137426c19f60b90490d15615cfe
KalevRK <KalevRK@gmail.com>
2015-02-16 15:24:34 -0800

    Display username and text for all messages from server.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,27 @@
 // YOUR CODE HERE:
 
+var display = function(data){
+  $messages = $("#messages");
+  _.each(data.results, function(item){
+    var username = '<div class="username">' + item.username + '</div>';
+    var text = '<div class="text">' + item.text + '</div>';
+    // console.dir(data);
+    // console.dir(item);
+    // console.log("username: " + username);
+    // console.log("text: " + text);
+    $messages.append("<div class='message'>"+username+text+"</div>");
+  });
+
+};
+
+$.ajax({
+  url: 'https://api.parse.com/1/classes/chatterbox',
+  type: 'GET',
+  contentType: 'application/json',
+  success: function(data){
+    display(data);
+  },
+  error: function(data){
+    console.error("GET error");
+  }
+});
