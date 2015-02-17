5c0b3a45543bccaed9e1e99c2a6b2598f29dc8c7
KalevRK <KalevRK@gmail.com>
2015-02-16 17:27:26 -0800

    track list of rooms

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,11 +1,14 @@
 // YOUR CODE HERE:
 var app = {};
-
 app.server = 'https://api.parse.com/1/classes/chatterbox';
+app.order = 'order=-createdAt';
+app.rooms = [];
 
 app.display = function(data){
+  x= data;
   $messages = $("#messages");
   $messages.html('');
+  // console.dir(data);
   _.each(data.results, function(item){
     var username = '<div class="username">@' + item.username + '</div>';
     var timestamp = '<div class="createdAt">' + item.createdAt + '</div>';
@@ -19,18 +22,25 @@ app.display = function(data){
   });
 };
 
-app.fetch = function(){
+app.getRooms = function(data) {
+  app.rooms = _.uniq(_.pluck(data.results, 'roomname'));
+};
+
+app.fetch = function(order){
   $.ajax({
-    url: this.server+'?order=-createdAt',
+    url: this.server+'?'+order,
     type: 'GET',
     contentType: 'application/json',
     success: function(data){
       app.display(data);
+      app.getRooms(data);
     },
     error: function(data){
       console.error("GET error");
     }
   });
+
+
 };
 
 app.send = function() {
@@ -53,9 +63,9 @@ app.send = function() {
 };
 
 app.init = function(){
-  this.fetch();
+  this.fetch(this.order);
   // setInterval(this.send.bind(this), 5);
-  setInterval(this.fetch.bind(this),1000);
+  setInterval(this.fetch.bind(this, this.order),1000);
 };
 
 
